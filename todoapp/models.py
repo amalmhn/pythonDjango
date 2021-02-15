f98acc8470bad7569f58a9caa1695ee068f4695f
Amal M <amalmhnofficial@gmail.com>
2021-02-15 20:49:19 +0530

    orm rivision going

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+#task name , date , status ,
+
+class Tasks(models.Model):
+    task_name=models.CharField(max_length=200)
+    date=models.CharField(max_length=20)
+    status=models.CharField(max_length=60)
+#insert into tasks values(bill payment,12/2/20,Not completed)
+#this is django ORM
+#ob = Tasks(task_name='bill payment',date='16/2/21',status='not completed')
+#ob.save()
+#tasks=Tasks.objects.all()
+
+    def __str__(self): #this is standard.You should always put this
+        return self.task_name
